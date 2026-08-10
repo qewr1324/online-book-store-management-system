f5ba43c83db5fdd8ba83f9122112f9de5f8a2645
qewr1324 <naiad.arts.studio@gmail.com>
2026-08-10 23:51:01 +0330

    fix(db.sql): fix db.sql query

--- app/src/main/resources/db/db.sql
+++ app/src/main/resources/db/db.sql
@@ -1,4 +1,4 @@
--- Active: 1780413880560@@127.0.0.1@5432@arash
+-- Active: 1780413880560@@127.0.0.1@5432@online_book_store_management_system
 CREATE DATABASE online_book_store_management_system;
 
 DROP DATABASE online_book_store_management_system;
@@ -9,8 +9,14 @@ SELECT * FROM books;
 
 SELECT * FROM publishers;
 
-SELECT bo.*, au.first_name || ' ' || au.last_name
-FROM books bo
-    INNER JOIN authors au ON bo.id = au.book_id;
+-- SELECT bo.*, au.first_name || ' ' || au.last_name
+-- FROM books bo
+--     INNER JOIN authors au ON bo.id = au.book_id;
+
+SELECT bo.*, au.first_name || ' ' || au.last_name AS author_fullname
+FROM
+    books bo
+    INNER JOIN books_authors bo_au ON bo.id = bo_au.books_id
+    INNER JOIN authors au ON bo_au.authors_id = au.id;
 
 -- books, authors(book_id), profiles(author_id), publishers(author_id)
